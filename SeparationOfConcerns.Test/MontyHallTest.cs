32557f8f0fd082d8efdd85ffeb86a5292dbe8618
Basil Muff <basil_muff@sluz.ch>
2025-01-10 11:04:34 +0100

    Monty

--- SeparationOfConcerns.Test/MontyHallTest.cs
+++ SeparationOfConcerns.Test/MontyHallTest.cs
@@ -1,9 +1,105 @@
+
+
+
 namespace SeparationOfConcerns.Test;
 
-public class MontyHallTest
+
+public class MontyHallTests
 {
     [Fact]
-    public void Test()
+    public void Play_WithNegativeTimes_ThrowsArgumentException()
     {
+        // Arrange
+        int invalidTimes = -1;
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => MontyHall.Play(invalidTimes));
+        Assert.Equal("negative numbers are not supported", exception.Message);
+    }
+
+    [Fact]
+    public void StickingWins_WithCorrectGuess_ReturnsTrue()
+    {
+        // Arrange
+        var doors = new Dictionary<int, bool> { { 1, false }, { 2, true }, { 3, false } };
+        var playerGuess = 2;
+
+        // Act
+        var result = MontyHall.StickingWins(doors, new HashSet<int> { 1, 2, 3 }, playerGuess);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void StickingWins_WithIncorrectGuess_ReturnsFalse()
+    {
+        // Arrange
+        var doors = new Dictionary<int, bool> { { 1, false }, { 2, true }, { 3, false } };
+        var playerGuess = 1;
+
+        // Act
+        var result = MontyHall.StickingWins(doors, new HashSet<int> { 1, 2, 3 }, playerGuess);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void PrepareDoors_AssignsOnePriceToARandomDoor()
+    {
+        // Act
+        var doors = MontyHall.PrepareDoors();
+
+        // Assert
+        var prizeDoorCount = doors.Values.Count(value => value == true);
+        Assert.Equal(1, prizeDoorCount); // Only one door should have a price
+    }
+
+    [Fact]
+    public void RemoveLosingDoor_RemovesCorrectLosingDoor()
+    {
+        // Arrange
+        var doors = new Dictionary<int, bool> { { 1, false }, { 2, true }, { 3, false } };
+        var playerGuess = 2; // Player guesses door 2, which has the prize
+
+        // Act
+        var remainingChoices = MontyHall.RemoveLosingDoor(doors, playerGuess);
+
+        // Assert
+        Assert.Contains(2, remainingChoices); // Player's choice should remain
+        Assert.DoesNotContain(1, remainingChoices); // Door 1 should be removed as a losing door
+        Assert.Contains(3, remainingChoices); // Door 3 should be removed as a losing door
+    }
+
+    [Fact]
+    public void GetRandomInt_ReturnsValidDoorNumber()
+    {
+        // Act
+        var result = MontyHall.GetRandomInt();
+
+        // Assert
+        Assert.InRange(result, 1, 3); // Result should be between 1 and 3
+    }
+
+    [Fact]
+    public void Play_PrintsCorrectResults_WhenPlayedMultipleTimes()
+    {
+        // Arrange
+        var timesToPlay = 100;
+
+        // Capture console output
+        using (var sw = new StringWriter())
+        {
+            Console.SetOut(sw);
+
+            // Act
+            MontyHall.Play(timesToPlay);
+
+            // Assert
+            var output = sw.ToString();
+            Assert.Contains($"played {timesToPlay} times", output);
+            Assert.Contains("won", output); // Checking for winning stats
+        }
     }
 }
--- SeparationOfConcerns/MontyHall.cs
+++ SeparationOfConcerns/MontyHall.cs
@@ -7,48 +7,73 @@ public class MontyHall
             throw new ArgumentException("negative numbers are not supported");
         }
         var wonSticking = 0;
-        var wonChanging = 0;
         for (var i = 0; i < times; i++)
         {
             // first, prepare the game
-            var doorsWithPrice = new Dictionary<int, bool> { { 1, false }, { 2, false }, { 3, false } };
-            var winningDoor = (int)(new Random().NextInt64() % 3 + 1);
-            doorsWithPrice[winningDoor] = true;
+            var doorsWithPrice = PrepareDoors();
 
             // second, let the player make his guess
-            var playerGuess = (int)(new Random().NextInt64() % 3 + 1);
+            var playerGuess = GetRandomInt();
+
 
             // third, reveal a losing door to be eliminated from the choices
-            var choices = doorsWithPrice.Keys.ToHashSet();
-            for (var j = 1; j <= 3; j++)
-            {
-                if (j != playerGuess && doorsWithPrice[j] == false)
-                {
-                    choices.Remove(j);
-                    break;
-                }
-            }
+            var choices = RemoveLosingDoor(doorsWithPrice, playerGuess);
+
 
             // fourth, count wins by 1) sticking to choice, and 2) changing choice
-            var winsSticking = doorsWithPrice[playerGuess];
-            choices.Remove(playerGuess);
-            var otherDoor = choices.First();
-            var winsChanging = doorsWithPrice[otherDoor];
-            if (winsSticking)
+            var stickingWins = StickingWins(doorsWithPrice, choices, playerGuess);
+
+            if (stickingWins)
             {
                 wonSticking++;
-            }
-            if (winsChanging)
-            {
-                wonChanging++;
-            }
+            } 
         }
 
         // finally, print the statistics
+        PrintResults(times, wonSticking);
+    }
+
+    public static bool StickingWins(Dictionary<int, bool> doorsWithPrice, HashSet<int> choices, int playerGuess)
+    {
+        return doorsWithPrice[playerGuess];
+    }
+
+    public static void PrintResults(int times, int wonSticking)
+    {
+        var wonChanging = times - wonSticking;
+
         Console.WriteLine($"played {times} times");
         Console.WriteLine($"won {wonSticking} times by sticking to choice");
         Console.WriteLine($"won {wonChanging} times by changing the choice");
         Console.WriteLine($"sticking wins {wonSticking / (float)times * 100:0.00}% of games");
-        Console.WriteLine($"changing wins {wonChanging / (float)times * 100:0.00}% of games");
+        Console.WriteLine($"changing wins {(wonChanging) / (float)times * 100:0.00}% of games");
+    }
+
+    public static Dictionary<int, bool> PrepareDoors()
+    {
+        var doorsWithPrice = new Dictionary<int, bool> { { 1, false }, { 2, false }, { 3, false } };        
+        doorsWithPrice[GetRandomInt()] = true;
+
+        return doorsWithPrice;
+    }
+
+    public static int GetRandomInt()
+    {
+        return (int)(new Random().NextInt64() % 3 + 1);
+    }
+
+    public static HashSet<int> RemoveLosingDoor(Dictionary<int, bool> doors, int playerGuess) 
+    {
+        var choices =  doors.Keys.ToHashSet();
+
+        for (var j = 1; j <= 3; j++)
+        {
+            if (j != playerGuess && doors[j] == false)
+            {
+                choices.Remove(j);
+                break;
+            }
+        }
+        return choices;
     }
 }
